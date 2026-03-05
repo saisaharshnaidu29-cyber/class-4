f92f4b71d2312c4574a2ccd4490524bddf1d9591
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-05 16:37:51 +0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+amount =int(input("please enter the amount for withdraw"))
+note_1 = amount//100
+note_2 = (amount%100)//50
+note_3 = ((amount%100)%50)//10
+print("notes of 100 rupees" , note_1)
+print("notes of 50 rupees", note_2)
+print("notes of 10 rupees", note_3)
