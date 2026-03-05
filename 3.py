ef766c7071f53864025272b2f661618ded6711e6
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-05 16:38:13 +0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+print("enter marks obtained in 4 subjects")
+math = int(input("maths :"))
+science = int(input("science :"))
+english = int(input("english :"))
+hindi = int(input("hindi :"))
+sum = math + science + english + hindi
+print("math + science + english + hindi = ", sum)
+perc = (sum/400)*100
+print("percentage Mark = ")
+print(perc)
